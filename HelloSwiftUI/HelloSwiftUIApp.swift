a1525be753268195a60ee55254e747ec6d11f7d9
jaroshevskii <jaroshevskiisasha@gamil.com>
2023-06-30 23:40:21 +0300

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by MAC on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
